25fbee0f66a77314304875149e6c70ab941cc08f
Nalini Chawla <chawlanalini@yahoo.co.uk>
2016-04-09 17:25:50 +0100

    Example app.posts

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 /**
  * Created by Kamla Chawla on 4/9/2016.
  */
+
+http://103.23.18.8/post-slideshow
+
+app.post(
+    "/login",
+    function(){
+
+    }
+);
+
+app.post(
+    "/post-slidehow",
+    function(){
+
+    }
+);
