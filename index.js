139ea7c323d1559bf16ab76fb7145b433d1256d7
Nalini Chawla <chawlanalini@yahoo.co.uk>
2016-05-24 19:46:55 +0100

    saving username, password and title to 'Users' database in collection 'speakappuser' working. Database is 'Users'.

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var
     express = require('express'),
     cors = require('cors'),
     app = express(),
-    bodyParser = require('body-parser')
+    bodyParser = require('body-parser'),
+    MongoClient = require('mongodb').MongoClient
 ;
 
 app.use(cors());
@@ -18,13 +19,37 @@ app.use(bodyParser.urlencoded({
 
 console.log("hello");
 
+//var db;
 app.post(
     '/login',
     function (request, response) {
         console.log("REQUEST BODY IS " + JSON.stringify(request.body));
         console.log("REQUEST PATH IS " + request.path);
         response.json({name: "Nalini"});
-    }
-);
+        MongoClient.connect('mongodb://localhost/Users', function (err, db) {
+            if (err) {
+                return console.dir(err);
+
+            }
+            var collection = db.collection('speakappuser');
+            collection.insertMany(request.body, {w:1}, function(err, result) {});
+            collection.find({title: 'great'}).toArray(function(err, result)
+            {
+                if (err) {
+                    console.log(err);
+                } else if (result.length) {
+                    console.log('Found: ', result);
+                } else {
+                    console.log('No document(s) found with defined "find" criteria');
+                }
+
+
+                db.close();
+            });
+        });
+
+    });
 
 app.listen(5000);
+app.listen(27017);
+
